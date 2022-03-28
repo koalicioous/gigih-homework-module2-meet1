4041a8d27ae8340715fee60189650ad800e3913a
Ulil Albab <aoohan@Ulils-MacBook-Pro.local>
2022-03-28 13:08:41 +0700

    Loop rendering

--- src/components/Track/index.js
+++ src/components/Track/index.js
@@ -1,4 +1,4 @@
-const Track = ({number,title,artist}) => {
+const Track = ({number,title,artist,album}) => {
     return (
         <div>
             <div className="grid grid-cols-[50px_1fr_80px] gap-4 text-white mb-3">
@@ -7,7 +7,12 @@ const Track = ({number,title,artist}) => {
                 </div>
                 <div className="text-left">
                     <h3 className="font-semibold">{title}</h3>
-                    <p className="text-gray-300">{artist}</p>
+                    <div>
+                        {/* <span className="text-gray-300">{artist}</span>
+                        <span className="mx-4">–</span> */}
+                        <span className="text-gray-300">{album}</span>
+
+                    </div>
                 </div>
                 <div className="flex items-center justify-center">
                     <button className="transition-all py-2 px-5 text-sm border border-gray-600 hover:border-gray-400 rounded-full">
--- src/components/Tracks/index.js
+++ src/components/Tracks/index.js
@@ -1,37 +1,21 @@
 import Container from "../Container";
 import Track from "../Track";
+import songs from '../../content/tracks';
 
-const songs = [
-    {
-        title: 'Tujuh Belas',
-        artist: 'Tulus',
-    },
-    {
-        title: 'Kelana',
-        artist: 'Tulus',
-    },
-    {
-        title: 'Remedi',
-        artist: 'Tulus',
-    },
-    {
-        title: 'Interaksi',
-        artist: 'Tulus',
-    },
-    {
-        title: 'Ingkar',
-        artist: 'Tulus',
-    },
-    {
-        title: 'Jatuh Suka',
-        artist: 'Tulus',
-    },
-]
 const Tracks = () => {
     return (
         <section className="py-8 bg-gradient-to-b from-gray-700 to-gray-800 flex-grow">
             <Container>
-                {songs.map((song,idx) => <Track number={idx} title={song.title} artist={song.artist} />)}
+                {songs.map(
+                    (song,idx) => (
+                        <Track
+                            number={idx}
+                            title={song.name}
+                            artist={song.artists[0].name}
+                            album={song.album.name}
+                        />
+                    )
+                )}
             </Container>
         </section>
     )

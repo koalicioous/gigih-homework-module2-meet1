3fe89d1171e7057afed1198a65698c0b3585cc2f
Ulil Albab <aoohan@Ulils-MacBook-Pro.local>
2022-03-30 00:16:30 +0700

    add spotify search functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import data from './content/album'
-import Header from './components/Header';
-import Tracks from './components/Tracks';
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Tracks from './components/Tracks'
 import './App.css';
+import { SearchProvider, useSearchResult } from './context/useSearchResult';
 
 function App() {
-  console.log(data)
+  const { result } = useSearchResult()
   return (
     <div className="App flex flex-col min-h-screen">
-      <Header
-        imageUrl={data.album.images[0].url}
-        albumName={data.album.name}
-      />
-      <Tracks />
-      <div className='text-xs bg-gray-800 py-3 text-white'>
-        Generasi Gigih Frontend Track Homework by Muhammad Ulil Albab Surya Negara
-      </div>
+        <Navbar />
+        {
+          result.length === 0 &&
+          <Header
+            imageUrl={data.album.images[0].url}
+            albumName={data.album.name}
+          />
+        }
+        <Tracks />
+        <div className='text-xs bg-gray-800 py-3 text-white'>
+          Generasi Gigih Frontend Track Homework by Muhammad Ulil Albab Surya Negara
+        </div>
     </div>
   );
 }
 
-export default App;
+const AppContainer = () => {
+  return (
+    <SearchProvider>
+      <App />
+    </SearchProvider>
+  )
+}
+
+export default AppContainer;
--- src/components/Tracks/index.js
+++ src/components/Tracks/index.js
@@ -1,14 +1,18 @@
 import Container from "../Container";
 import Track from "../Track";
 import songs from '../../content/tracks';
+import { useSearchResult } from "../../context/useSearchResult";
 
 const Tracks = () => {
+    const { result } = useSearchResult()
+
     return (
         <section className="py-8 bg-gradient-to-b from-gray-700 to-gray-800 flex-grow">
             <Container>
-                {songs.map(
+                {result.length === 0 && songs.map(
                     (song,idx) => (
                         <Track
+                            key={idx}
                             number={idx}
                             title={song.name}
                             artist={song.artists[0].name}
@@ -16,6 +20,19 @@ const Tracks = () => {
                         />
                     )
                 )}
+                {
+                    result.length > 0 && result.map(
+                        (song,idx) => (
+                            <Track
+                                key={idx}
+                                number={idx}
+                                title={song.name}
+                                artist={song.artists[0].name}
+                                album={song.album.name}
+                            />
+                        )
+                    )
+                }
             </Container>
         </section>
     )
